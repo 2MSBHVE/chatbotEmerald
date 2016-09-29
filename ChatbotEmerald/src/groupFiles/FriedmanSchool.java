be43f8ba0b7d642b409716675611bbee18cd70e7
Max@School <Tech Squad@10.8.33.104>
2016-09-29 13:55:32 -0400

    template added

---
+++ ChatbotEmerald/src/groupFiles/FriedmanSchool.java
@@ -0,0 +1,33 @@
+package groupFiles;
+
+public class FriedmanSchool implements Chatbot {
+
+	private boolean inSchoolLoop;
+	private String schoolResponse;
+	
+
+	public void talk() {
+		inSchoolLoop = true;
+		while(inSchoolLoop){
+			FriedmanMain.println("(Type 'quit' to go back.)");
+			schoolResponse = FriedmanMain.promptInput("");
+			if(schoolResponse.indexOf("quit") >= 0){
+				inSchoolLoop = false;
+				FriedmanMain.promptInputForever("");
+			}
+			FriedmanMain.println("That's my favorite part about school!");
+		}
+		
+	}
+
+	public boolean isTriggered(String userInput) {
+//		String[] triggers = {"school", "class", "teacher"};
+//		for(int i = 0; ){}
+		
+		if(FriedmanMain.findKeyword(userInput, "school", 0) >= 0){
+			return true;
+		}
+		return false;
+	}
+
+}
